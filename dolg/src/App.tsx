56bd7cc19b8220f29982c8723a377557a634925c
Petrik Diak <diak@petrik.hu>
2024-12-04 15:51:34 +0100

    end of class fragments. return value is not there :(

--- dolg/src/App.tsx
+++ dolg/src/App.tsx
@@ -1,23 +1,62 @@
-interface product{
+import './App.css'
+import React, { useState, useRef } from 'react'
+
+interface Product{
   id: number, 
   name: string,
   amount: number,
   unit: string,
   bought: boolean
 }
-import './App.css'
-const productList:product[] = []
+const productList:Product[] = []
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [newProductName, setNewProductName] = useState("");
+  const [newProductAmount, setNewProductAmount] = useState("");
+  const [newProductUnit, setNewProductUnit] = useState("");
+  const [error, setError] = useState('');
+  const [remaining, setRemaining] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null); //inputref.current.focus() vhova tudja tenni a kurzort/fókuszt
 
-  const exampleProduct:product = {
+  const addProduct = () => 
+    
+    {
+
+      const newProduct = { id: 0, name:newProductName, amount:newProductAmount, unit:newProductUnit, bought: false}
+      setProducts((prevProducts) => [...prevProducts, newProduct]);
+      setNewProductName("");
+      setNewProductAmount("");
+      setNewProductUnit("");
+      setError("");
+      setRemaining(remaining+1)
+
+    } 
+    //TBA!! 
+    //ID WRONG CORRECT LATER
+  //hiba: üres érték bármire
+  //hiba: már meglevő termék
+
+  const toggleProductComletion = (productID: number) => {
+    //t.úr három függvényt hívó függvénnyel csinálta
+    //funkcionális a nyavalya
+  }
+
+  const removeProduct = (productID: number) =>{
+    //filter(), azokra az idkra, amik nincsenek benne a dologban
+    //todo: nézz utána a filternek
+  }
+
+
+
+  const exampleProduct:Product = {
     id: 0,
-    name: "a",
-    amount: 1,
-    unit: "b",
+    name: "alma",
+    amount: 3,
+    unit: "db",
     bought: false
   }
-   productList.push(exampleProduct)
+  productList.push(exampleProduct)
   MakeTable();
 
 
@@ -28,6 +67,7 @@ function App() {
 }
 function MakeTable(){
   const tableContent = document.getElementById("dynamicTable");
+  tableContent!.innerHTML = "";
   productList.forEach(product => {
     const newLine = `
     <tr>
